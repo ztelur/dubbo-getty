37ba965104ff36a89cb9283506c30bcc519558a5
watermelon <80680489@qq.com>
2021-02-28 23:57:44 +0800

    fix unit test

--- client.go
+++ client.go
@@ -35,7 +35,6 @@ import (
 	"github.com/dubbogo/gost/net"
 	gxsync "github.com/dubbogo/gost/sync"
 	gxtime "github.com/dubbogo/gost/time"
-
 	"github.com/gorilla/websocket"
 	perrors "github.com/pkg/errors"
 )
